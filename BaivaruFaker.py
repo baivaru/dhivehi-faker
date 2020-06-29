f9a94c53880979401d445f623f938a555d5a7ad5
Mohamed Arham <c4.arham@gmail.com>
2020-06-29 15:50:10 +0500

    - More customizations

--- BaivaruFaker.py
+++ BaivaruFaker.py
@@ -2,9 +2,11 @@ import json
 from random import choice, randint
 
 class BaivaruFaker:
-    def __init__(self, words_file="thaana.json", endings_file="end.json"):
+    def __init__(self, words_file="thaana.json", endings_file="end.json", sentence_length=randint(5,10), paragraph_length=randint(5,15)):
         self.word_list = json.load(open(words_file, encoding="utf-8"))
         self.endings_list = json.load(open(endings_file, encoding="utf-8"))
+        self.sentence_length = sentence_length
+        self.paragraph_length = paragraph_length
 
     def word(self):
         word = choice(self.word_list)
@@ -12,37 +14,37 @@ class BaivaruFaker:
 
     def words(self, count, array=False):
         if array:
-            return [self.word() for _ in range(1,count)]
+            return [self.word() for _ in range(0,count)]
 
         words = ""
-        for _ in range(1, count):
+        for _ in range(0, count):
             words += f"{self.word()} "
         return words
 
     def sentence(self):
-        length = randint(5,15)
+        length = self.sentence_length
         sentence = f"{self.words(length)} {choice(self.endings_list)}. "
         return sentence
 
-    def sentences(self, count, array=False):
+    def sentences(self, count=randint(5,20), array=False):
         if array:
-            return [self.sentence() for _ in range(1, count)]
+            return [self.sentence() for _ in range(0, count-1)]
 
         sentences = ""
-        for _ in range(1, count):
+        for _ in range(0, count):
             sentences += self.sentence()
         return sentences
 
     def paragraph(self):
-        length = randint(5, 15)
+        length = self.paragraph_length
         paragraph = self.sentences(length)
         return paragraph
     
-    def paragraphs(self, count, array=False):
+    def paragraphs(self, count=randint(5,10), array=False):
         if array:
-            return [self.paragraph() for _ in range(1, count)]
+            return [self.paragraph() for _ in range(0, count)]
 
         paragraphs = ""
-        for _ in range(1, count):
+        for _ in range(0, count):
             paragraphs += f"{self.paragraph()}\n\n"
-        return paragraphs
+        return paragraphs.rstrip()
