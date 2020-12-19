40f9318e2b89bf5c747d0e13737b39719097a6a0
Mohamed Arham <c4.arham@gmail.com>
2020-12-19 16:40:46 +0500

    - Fixed sentence ends

--- faker.py
+++ faker.py
@@ -23,7 +23,7 @@ class BaivaruFaker:
 
     def sentence(self):
         length = self.sentence_length
-        sentence = f"{self.words(length)} {choice(self.endings_list)}. "
+        sentence = f"{self.words(length)} {choice(self.endings_list)}."
         return sentence
 
     def sentences(self, count=randint(5,20), array=False):
@@ -32,7 +32,7 @@ class BaivaruFaker:
 
         sentences = ""
         for _ in range(count):
-            sentences += self.sentence()
+            sentences += self.sentence() + " "
         return sentences
 
     def paragraph(self):
