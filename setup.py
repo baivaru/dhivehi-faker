d350c53a8b10a2ae1df14e3d79c26ca6afc8c4f8
Athfan Khaleel <athphane@gmail.com>
2021-05-13 21:39:03 +0500

    - pypie tutorial

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='dhivehi-faker-baivaru',
     version='0.1.0',
     author="Baivaru",
     author_email="admin@baivaru.net",
     description="It's like Faker but Dhivehi",
-    long_description=open('README.md').read(),
-    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/baivaru/dhivehi-faker",
+    project_urls={
+        "Bug Tracker": "https://github.com/baivaru/dhivehi-faker/issues",
+    },
+    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
     package_data={'': ['*.json', 'data/*.json']},
     include_package_data=True,
-    install_requires=[],
-    url='https://github.com/baivaru/dhivehi-faker',
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent",
