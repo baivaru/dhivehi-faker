7f0ad1dbfe97322d42957fe4ec5ecd3247ee1f01
Athfan Khaleel <athphane@gmail.com>
2021-05-13 21:35:03 +0500

    - update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DhivehiFaker',
+    name='dhivehi-faker-baivaru',
     version='0.1.0',
     author="Baivaru",
     author_email="admin@baivaru.net",
